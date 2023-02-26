8852032dd2e3c3768c2ff52359166fa061035254
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2023-02-26 10:45:10 -0300

    Update Banana.cs

--- Smaug3/Assets/_Game/_Scripts/Entities/Banana/Banana.cs
+++ Smaug3/Assets/_Game/_Scripts/Entities/Banana/Banana.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Banana : MonoBehaviour
 {
     // Informações que irão ser retiradas do banco de dados 
-    [HideInInspector] public string Name { get { return Name; } private set { Name = value; } }
-    [HideInInspector] public int Damage { get { return Damage; } private set { Damage = value; } }
-    [HideInInspector] public float Speed { get { return Speed; } private set { Speed = value; } }
-    [HideInInspector] public int EnergyCost {get {return  EnergyCost;} private set {Speed = value;} }
+    [HideInInspector] public string Name { get; private set; }
+    [HideInInspector] public int Damage { get; private set; }
+    [HideInInspector] public float Speed { get; private set; }
+    [HideInInspector] public int EnergyCost {get; private set; }
 
     // TODO: Integração entre o SQL e C#
 }
